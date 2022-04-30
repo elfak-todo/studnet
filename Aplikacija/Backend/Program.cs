1f0fad64b1c8fd6178ec213ec028b127b73b22c8
milan-lukic <milan.lukic@elfak.rs>
2022-04-30 15:21:58 +0200

    wip

--- Aplikacija/Backend/Program.cs
+++ Aplikacija/Backend/Program.cs
@@ -1,11 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CORS",
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:8080",
+                                              "https://localhost:8080",
+                                              "http://127.0.0.1:8080",
+                                              "https://127.0.0.1:8080",
+                                              "https://127.0.0.1:5500",
+                                              "http://127.0.0.1:5500",
+                                              "http://127.0.0.1:5500",
+                                              "http://localhost:5500",
+                                              "https://localhost:5500")
+                                              .AllowAnyHeader()
+                                              .AllowAnyMethod();
+                      });
+
+});
 
 var app = builder.Build();
 
@@ -17,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CORS");
 
 app.UseAuthorization();
 
