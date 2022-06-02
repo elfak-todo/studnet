b778a110f83913d36ea25502d81b0964129a5773
lukakocic <luka.kocic@elfak.rs>
2022-06-02 19:07:58 +0200

    wip

--- Aplikacija/frontend/src/App.js
+++ Aplikacija/frontend/src/App.js
@@ -1,18 +1,21 @@
 import NavigationBar from "./components/navbar/NavigationBar";
 import AppRoutes from "./AppRoutes";
 import AxiosConfig from "./AxiosConfig";
-
-import { StudentManager } from "./components/studentManager/StudentManager";
+import { Suspense } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./scss/theme.scss";
+import { StudentManager } from "./components/studentManager/StudentManager";
+import LoadingSpinner from "./components/loadingSpinner/LoadingSpinner.js";
 
 function App() {
   return (
     <StudentManager>
       <AxiosConfig />
       <NavigationBar />
-      <AppRoutes />
+      <Suspense fallback={<LoadingSpinner />}>
+        <AppRoutes />
+      </Suspense>
     </StudentManager>
   );
 }
