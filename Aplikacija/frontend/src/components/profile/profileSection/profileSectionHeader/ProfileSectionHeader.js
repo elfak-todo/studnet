6e37cbf9aaa954778ace13c66e0088621ef2a0af
Milan Lukic <milan.lukic@elfak.rs>
2022-06-13 17:45:37 +0200

    -wip

--- Aplikacija/frontend/src/components/profile/profileSection/profileSectionHeader/ProfileSectionHeader.js
+++ Aplikacija/frontend/src/components/profile/profileSection/profileSectionHeader/ProfileSectionHeader.js
@@ -78,29 +78,31 @@ function ProfileSectionHeader({ studentProp, setStudentProp }) {
         setShowEditCover={setShowEditCover}
       />
       <Card.Body>
-        <div>
-        <div className="header-desc">
-          <h2 className="text-name">
-            {`${studentProp?.firstName} ${studentProp?.lastName}`}
-          </h2>
-          <h4 className="m-0"> {studentProp?.universityName} </h4>
-          <Card.Text> {studentProp?.facultyName} </Card.Text>
-          <div className="header-badges">
-            <Badge className="p-2">
-              <FontAwesomeIcon icon={faClipboard} className="me-1" />
-              {`${t("misc:posts")} ${studentProp?.postCount}`}
-            </Badge>
-            <Badge className="ms-1 p-2">
-              <FontAwesomeIcon icon={faCalendar} className="me-1" />
-              {`${t("misc:events")} ${studentProp?.eventCount}`}
-            </Badge>
-            <Badge className="ms-1 p-2">
-              <FontAwesomeIcon icon={faMap} className="me-1" />
-              {`${t("misc:locations")} ${studentProp?.locationCount}`}
-            </Badge>
-          </div>
+        <div className="float-end">
+          <StudentSettings studentProp={studentProp} />
         </div>
-        <StudentSettings studentProp={studentProp}/>
+        <div>
+          <div className="header-desc">
+            <h2 className="text-name">
+              {`${studentProp?.firstName} ${studentProp?.lastName}`}
+            </h2>
+            <h4 className="m-0"> {studentProp?.universityName} </h4>
+            <Card.Text> {studentProp?.facultyName} </Card.Text>
+            <div className="header-badges">
+              <Badge className="p-2">
+                <FontAwesomeIcon icon={faClipboard} className="me-1" />
+                {`${t("misc:posts")} ${studentProp?.postCount}`}
+              </Badge>
+              <Badge className="ms-1 p-2">
+                <FontAwesomeIcon icon={faCalendar} className="me-1" />
+                {`${t("misc:events")} ${studentProp?.eventCount}`}
+              </Badge>
+              <Badge className="ms-1 p-2">
+                <FontAwesomeIcon icon={faMap} className="me-1" />
+                {`${t("misc:locations")} ${studentProp?.locationCount}`}
+              </Badge>
+            </div>
+          </div>
         </div>
       </Card.Body>
     </Card>
