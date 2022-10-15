30d11d30f639d1260bb890d21b43e95e2bec7887
bazlooka <kocicluka1@gmail.com>
2022-10-15 16:58:39 +0200

    API url updated

--- Aplikacija/frontend/src/config.js
+++ Aplikacija/frontend/src/config.js
@@ -1,5 +1,4 @@
 export const SERVER_ADDRESS = "https://todo-studnet.azurewebsites.net/";
-
 //export const SERVER_ADDRESS = "http://192.168.1.35:5000/";
 //export const SERVER_ADDRESS = "http://192.168.100.2:5000/";
 //export const SERVER_ADDRESS = "http://192.168.1.101:5000/";
